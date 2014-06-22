9014933fe2a39cccb81e90ffaf4e2138988abf0e
rocky_manobi <tak.koroki@gmail.com>
2014-06-22 18:33:04 +0900

    port

--- web.js
+++ web.js
@@ -17,6 +17,7 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
+var port = Number(process.env.PORT || 3000);
+http.listen(port, function(){
   console.log('listening on *:3000');
 });
