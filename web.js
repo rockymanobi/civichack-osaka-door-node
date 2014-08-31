9101f9906402a9b029f2488dc69964be42e3cb46
rocky_manobi <tak.koroki@gmail.com>
2014-08-31 10:39:33 +0900

    door追加

--- web.js
+++ web.js
@@ -1,9 +1,34 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var ECT = require('ect');
+var path = require('path');
+
+var ectRenderer = ECT({ watch: true, root: __dirname , ext : '.html' });
+app.engine('html', ectRenderer.render);
+app.set('views', path.join(__dirname));
+app.set('view engine', 'html');
+
+
+var Status = "???"
+
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
+});
+
+app.get('/sync',function(req, res){
+  var statusText = {
+    "0" : "open",
+    "1" : "close",
+  }[req.query.status];
+  Status = statusText; // 手抜き
+  io.emit('status_changed', statusText);
+  res.send("done");
+})
+app.get('/door', function(req, res){
+  res.render('door',{status: Status});
+  //res.sendfile('door.html');
 });
 
 
